6bdca67f976c0155dc20b49e8fc35328ba71d2f4
Alexandra Kung <ak697@cornell.edu>
2019-06-20 10:52:15 -0500

    trying to connect to database

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const main = require('./views/main')
+const { db } = require('./models');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
+
+db.authenticate().
+  then(() => {
+    console.log('connected to the database');
+  })
+
 
 app.get('/', (req, res) => {
   res.send(main());
@@ -15,3 +22,4 @@ const PORT = 1337;
 app.listen(PORT, () => {
   console.log(`app is listening on port ${PORT}`);
 });
+
